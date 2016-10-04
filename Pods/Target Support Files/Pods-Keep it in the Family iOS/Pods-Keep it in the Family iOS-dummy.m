240bbe66a48486e9aa5ce055d0ff585de9969c4c
Ken V <kvaczi2@gmail.com>
2016-10-03 19:16:45 -0500

    adding pod info

---
+++ Pods/Target Support Files/Pods-Keep it in the Family iOS/Pods-Keep it in the Family iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Keep_it_in_the_Family_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_Keep_it_in_the_Family_iOS
+@end
---
+++ Pods/Target Support Files/Pods-Keep it in the Family iOS/Pods-Keep it in the Family iOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Keep_it_in_the_Family_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Keep_it_in_the_Family_iOSVersionString[];
+
